86cfe236ab9454ed96b16ebdcd03b1df1c10e626
amirhosein <aaskari379hh@gmail.com>
2026-07-17 15:43:44 +0330

    superuser code change

--- home_app/management/commands/create_admin.py
+++ home_app/management/commands/create_admin.py
@@ -1,12 +1,20 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        User.objects.create_superuser(
-            username='admin',
-            email='aaskari379hh@email.com',
-            password='amiroo10'
-        )
-        self.stdout.write("Admin created")
+        username = "admin"
+        password = "12345678"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email="admin@example.com",
+                password=password
+            )
+            self.stdout.write("Admin created")
+        else:
+            self.stdout.write("Admin already exists")
